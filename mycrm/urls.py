389777ba8a82a59b04d2306a8aca93ad8243bd7c
Chukwuma Obasi <ccobasi8@gmail.com>
2023-11-26 16:23:01 +0100

    Fix redirect issues

--- mycrm/urls.py
+++ mycrm/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views
 from core.views import index
+from userprofile.views import signup
 
 urlpatterns = [
     path('', index, name="index"),
+    path('sign-up/', signup, name="signup"),
+    path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('log-out/', views.LoginView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
