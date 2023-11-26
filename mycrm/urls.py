781ed430f78d88643cb967ce843f80bb1d841b25
Chukwuma Obasi <ccobasi8@gmail.com>
2023-11-26 16:56:15 +0100

    Add dashboard

--- mycrm/urls.py
+++ mycrm/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views
 from core.views import index
 from userprofile.views import signup
 
 urlpatterns = [
     path('', index, name="index"),
+    path('dashboard/', include('dashboard.urls')),
     path('sign-up/', signup, name="signup"),
     path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
     path('log-out/', views.LoginView.as_view(), name='logout'),
