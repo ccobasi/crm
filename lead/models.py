949c4b5e8c7bfbcde87dbaa3081b873c38349b91
Chukwuma Obasi <ccobasi8@gmail.com>
2023-11-28 01:26:40 +0100

    Add lead app

---
+++ lead/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Lead(models.Model):
+    LOW = 'low'
+    MEDIUM = 'medium'
+    HIGH = 'high'
+
+    CHOICE_PRIORITY = (
+        (LOW, 'LOW'),
+        (MEDIUM, 'MEDIUM'),
+        (HIGH, 'HIGH'),
+    )
+
+    NEW = 'new'
+    CONTACTED = 'contacted'
+    WON = 'won'
+    LOST = 'lost'
+
+    CHOICE_STATUS = (
+        (NEW, 'new'),
+        (CONTACTED, 'contacted'),
+        (WON, 'won'),
+        (LOST, 'lost')
+    )
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    description = models.TextField(blank=True, null=True)
+    priority = models.CharField(max_length=10, choices=CHOICE_PRIORITY, default=MEDIUM)
+    status = models.CharField(max_length=10, choices=CHOICE_STATUS, default=NEW)
+    created_by = models.ForeignKey(User, related_name='leads', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
